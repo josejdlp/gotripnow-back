72d74ebc63b599071287983772b1cd8623023852
josejdelapaz <josejdlp@gmail.com>
2021-04-02 20:20:47 +0200

    Aplicando seguridad jwt

---
+++ src/main/java/com/jose/gotripnowback/security/enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.jose.gotripnowback.security.enums;
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/jose/gotripnowback/security/service/RolService.java
@@ -0,0 +1,27 @@
+package com.jose.gotripnowback.security.service;
+
+
+import com.jose.gotripnowback.security.entity.Rol;
+import com.jose.gotripnowback.security.enums.RolNombre;
+import com.jose.gotripnowback.security.repository.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class RolService {
+
+    @Autowired
+    RolRepository rolRepository;
+
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return rolRepository.findByRolNombre(rolNombre);
+    }
+
+    public void save(Rol rol){
+        rolRepository.save(rol);
+    }
+}
