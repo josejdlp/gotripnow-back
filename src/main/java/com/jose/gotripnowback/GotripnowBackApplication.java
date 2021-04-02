8548924a7a5997a380afdcd5126e0f624e49dd58
josejdelapaz <josejdlp@gmail.com>
2021-04-02 18:42:46 +0200

    Generando estructura

---
+++ src/main/java/com/jose/gotripnowback/GotripnowBackApplication.java
@@ -0,0 +1,13 @@
+package com.jose.gotripnowback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotripnowBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotripnowBackApplication.class, args);
+	}
+
+}
